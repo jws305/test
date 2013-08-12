f68788c929fd092d90793c451dcb1c2a6ccfcf5d
John Seaman <jseaman@metrostarsystems.com>
2013-08-12 13:56:44 -0400

    set up app.js for node

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-function MyApp() {
-	console.log("Hello world");
-}
-
-function Add(x, y) {
-	return x + y;
-}
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(80);
+console.log('Server running on port 80');
